a503cd6f65a6da4b8361745344dcb8a6582bbdd0
danelduranxs <107886582+danelduranxs@users.noreply.github.com>
2022-06-23 15:17:43 -0500

    Add files via upload

---
+++ Interes Simulator.py
@@ -0,0 +1,10 @@
+print()
+
+a=int(input("Money Ingress: $"))
+print()
+b=int(input("Interest rate selected: %"))
+print()
+c=int(input("Years to invest:"))
+
+print()
+print("Al cabo de",c,"años, su dinero será de $", a*(1+(b/100))**c)
